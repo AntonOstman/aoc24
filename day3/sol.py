3898e0a7558b66990762841bb0a5346930e7e3e1
Anton <anton.ostman123@gmail.com>
2024-12-03 11:01:53 +0100

    Clean up day 3 a bit

--- day3/sol.py
+++ day3/sol.py
@@ -1,20 +1,16 @@
-import os
-import subprocess
-import re
+# part1: grep 'mul([0-9]*,[0-9]*)' input -o | grep '[0-9]*,[0-9]*' -o > clean-input && python3 sol.py
+# part2: cat input2 | perl -pe "s/don\'t\(\).*?do\(\)//g" | grep 'mul([0-9]*,[0-9]*)' -o | grep '[0-9]*,[0-9]*' -o > clean-input && python3 sol.py
 
-# grep 'mul([0-9]*,[0-9]*)' input -o | grep '[0-9]*,[0-9]*' -o > clean-input
-# part2 cat input2 | perl -pe "s/don\'t\(\).*?do\(\)//g" | grep 'mul([0-9]*,[0-9]*)' -o | grep '[0-9]*,[0-9]*' -o > clean-input22 && python3 sol.py
-f = open("clean-input22")
+f = open("clean-input")
 
 lines = f.read().split("\n")[0:-1]
-
 
 total = 0
 for line in lines:
     numbers = line.split(",")
-    if len(numbers) < 2 or line == ',':
-        continue
     total += int(numbers[0]) * int(numbers[1])
+
+f.close()
 
 print(total)
 
