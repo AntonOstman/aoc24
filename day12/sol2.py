568d9b42503c4c47517ef1f723e51936fd15aa90
Anton <anton.ostman123@gmail.com>
2024-12-12 18:20:33 +0100

    Add day 12

--- day12/sol2.py
+++ day12/sol2.py
@@ -1,42 +1,10 @@
-
 from collections import defaultdict
-f = open("test2")
+f = open("input")
 
 lines = f.read().split("\n")[0:-1]
 
-total = 0
 graph = defaultdict(set)
-# perimiter = defaultdict(set)
-perimiter = set()
-num_nodes = 0
 perimiters = 0
-
-def get_adj2(node, map):
-    adj = []
-    dirs = []
-
-    # print(map)
-    for n in map:
-        if (n[0] + 1,n[1]) == node:
-            adj.append(n)
-            dirs.append("side")
-
-        if(n[0],n[1] + 1) == node:
-            adj.append(n)
-            dirs.append("up")
-
-        if (n[0] - 1,n[1]) == node:
-            adj.append(n)
-            dirs.append("side")
-
-        if (n[0], n[1] - 1) == node:
-            adj.append(n)
-            dirs.append("up")
-
-    # print('working on', node)
-    # print('adjecent', adj)
-    # print('dirs', dirs)
-    return (adj, dirs)
 
 def get_adj(node, map):
     adj = []
@@ -45,60 +13,23 @@ def get_adj(node, map):
     if node[0] + 1 < len(lines[0]) and map[node[1]][node[0] + 1] == map[node[1]][node[0]]:
         adj.append((node[0] + 1, node[1]))
     else:
-        # perimiters += 1
-        perimiter.add((node[0] + 1,node[1]))
+        perimiters += 1
 
     if node[1] + 1 < len(lines) and map[node[1] + 1][node[0]] == map[node[1]][node[0]]:
         adj.append((node[0], node[1] + 1))
     else:
-        # perimiters += 1
-        perimiter.add((node[0],node[1] + 1))
+        perimiters += 1
 
     if node[0] - 1 >= 0 and map[node[1]][node[0] - 1] == map[node[1]][node[0]]:
         adj.append((node[0] - 1, node[1]))
     else:
-        # perimiters += 1
-        perimiter.add((node[0] - 1,node[1]))
+        perimiters += 1
 
     if node[1] - 1 >= 0 and map[node[1] - 1][node[0]] == map[node[1]][node[0]]:
         adj.append((node[0], node[1] - 1))
     else:
-        # perimiters += 1
-        perimiter.add((node[0],node[1] - 1))
+        perimiters += 1
 
-    # Add diagonals to perimiters
-
-    if node[0] + 1 < len(lines[0]) and node[1] + 1 < len(lines) and map[node[1] + 1][node[0] + 1] == map[node[1]][node[0]]:
-        pass
-        # adj.append((node[0] + 1, node[1]))
-    else:
-        # perimiters += 1
-        perimiter.add((node[0] + 1,node[1] + 1))
-
-    if node[1] + 1 < len(lines) and node[0] - 1 >= 0 and map[node[1] + 1][node[0] - 1] == map[node[1]][node[0]]:
-        pass
-        # adj.append((node[0], node[1] + 1))
-    else:
-        # perimiters += 1
-        perimiter.add((node[0] - 1,node[1] + 1))
-
-    if node[1] - 1 >= 0 and node[0] + 1 < len(lines[0]) and map[node[1] - 1][node[0] + 1] == map[node[1]][node[0]]:
-        # adj.append((node[0] + 1, node[1] - 1))
-        pass
-    else:
-        # perimiters += 1
-        perimiter.add((node[0] + 1,node[1] - 1))
-
-    if node[1] - 1 >= 0 and node[0] - 1 >= 0 and map[node[1] - 1][node[0] - 1] == map[node[1]][node[0]]:
-        # adj.append((node[0], node[1] - 1))
-        pass
-    else:
-        # perimiters += 1
-        perimiter.add((node[0] - 1,node[1] - 1))
-
-    # print('working on', node)
-    # print('adjecent', adj)
-    # print('perimiters', perimiter)
     return adj
 
 def dfs(node, map, visited, graph):
@@ -111,70 +42,36 @@ def dfs(node, map, visited, graph):
             graph[node].add(neighour)
             dfs(neighour, map, visited, graph)
 
-def dfs2(node, map, visited, graph, prev_dir):
-    global perimiters
-    # if prev_dir is None:
-    #     perimiters += 1
-    if node not in visited:
-        # print('doing node', node)
-        # if prev_dir is not None:
-        #     visited.add(node)
-        visited.add(node)
-        # num_nodes += 1
-        
-        # neighours = get_adj2(node, graph);
-        # print(map)
-        neighours = get_adj2(node, map);
-        # print(neighours)
+def calc_corners(nodes):
+    corners = 0
+    for node in nodes:
+        for dx, dy in [(1, 1), (-1, 1), (1, -1), (-1, -1)]:
 
-        # print(neighours) 
-        # print(neighours[0]) 
-        # print(neighours[1]) 
-        # print('node', node, 'has', len(neighours))
-        # new_dirs = set()
-        # new_dirs.add(prev_dir)
-        for i in range(len(neighours[0])):
-            neighour = neighours[0][i]
-            # print('node', node, 'has adj', neighour)
-            dir = neighours[1][i]
-            # print('node and nourghjksd',node, neighour, dir)
+            # outside
+            if ((node[0] + dx, node[1]) not in nodes and (node[0], node[1] + dy) not in nodes):
+                corners += 1
 
-            # graph[node].add(neighour)
-            # print(dir, prev_dir)
-            # if dir not in new_dirs:
-                # new_dirs.add(dir)
-                # perimiters += 1
-                # print(node, neighour, prev_dir, dir)
-            if prev_dir is None:
-                perimiters += 1
-                dfs2(neighour, map, visited, graph, dir)
-                break
-            if prev_dir != dir and neighour not in visited:
-                perimiters += 1
-                print(node, neighour, prev_dir, dir)
-            dfs2(neighour, map, visited, graph, dir)
+            # inside
+            if ((node[0] + dx, node[1]) in nodes and (node[0], node[1] + dy) in nodes and (node[0] + dx, node[1] + dy) not in nodes):
+                    corners += 1
+    return corners
+
+p1 = 0
+p2 = 0
 
 for y, line in enumerate(lines):
     for x, letter in enumerate(line):
         if (x,y) in graph:
             continue
         num_nodes = 0
-        perimiters = 0
-        print('letter', letter)
-        dfs((x,y), lines, set(), graph)
-        start = next(iter(perimiter))
-        print('start', start)
-        print('perimiters', perimiter)
-        dfs2(start, perimiter, set(), defaultdict(set), None)
-        print('number of perimiters', len(perimiter), perimiters)
-        # calculate_line_segments(perimiter) 
-        # print(perimiters)
-        perimiter = set()
-        total += num_nodes * perimiters
-        num_nodes = 0
+        seen = set()
+        dfs((x,y), lines, seen, graph)
+        sides = calc_corners(seen)
+        p1 += len(seen) * perimiters
+        p2 += num_nodes * sides
         perimiters = 0
 
-
-print(total)
+print(p1)
+print(p2)
 
 f.close()
